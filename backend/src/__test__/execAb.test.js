220f0de918ac68c5ef0d81b7610d50ecfca28c8f
Shuto Takahahsi <m59rt@yahoo.co.jp>
2020-11-21 21:44:08 +0900

    Update not use condition in jest

--- backend/src/__test__/execAb.test.js
+++ backend/src/__test__/execAb.test.js
@@ -14,19 +14,11 @@ test("Can execute ab with request and concurrency params", async () => {
 test("Return caution with minus request", async () => {
   const requests = -1
   const concurrency = 10
-  try {
-    const results = await execAb({requests, concurrency, serverUrl})
-  } catch (e) {
-    expect(e).toBe('requests must greater than 0')
-  }
+  await expect(execAb({requests, concurrency, serverUrl})).toBe('requests must greater than 0')
 })
 
 test("Return caution with minus concurrency", async () => {
   const requests = 10
   const concurrency = -1
-  try {
-    const results = await execAb({requests, concurrency, serverUrl})
-  } catch (e) {
-    expect(e).toBe('concurrency must greater than 0')
-  }
+  await expect(execAb({requests, concurrency, serverUrl})).toBe('concurrency must greater than 0')
 })
